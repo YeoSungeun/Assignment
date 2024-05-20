16ff6b57712df674d11bfe508bb9c0cf4060277b
YeoSungeun <dutjddms@naver.com>
2024-05-20 23:14:08 +0900

    [Design] HomeView AutoLayout 적용

--- Netfilx/HomeViewController.swift
+++ Netfilx/HomeViewController.swift
@@ -57,8 +57,8 @@ class HomeViewController: UIViewController {
         
         setSubImageViewList()
         
-        setTop10Image()
-        shuffleTop10()
+//        setTop10Image()
+//        shuffleTop10()
         
         
     }
